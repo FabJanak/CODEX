f7a15f75e875dddbbdee91a8e0d8c87b57e5e9ce
Janak Fabyani <janakfabyani30036@gmail.com>
2023-09-08 18:34:10 +0530

    Add files via upload

---
+++ Q2code.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+int main(){
+    cout<<"the range of short int is="<<INT16_MIN<<"to"<<INT16_MAX<<endl;
+    cout<<"the range of long is="<<INT32_MIN<<"to"<<INT32_MAX<<endl;
+    cout<<"the range of char is="<<CHAR_MIN<<"to"<<CHAR_MAX<<endl;
+    cout<<"the range of double is="<<__DBL_MIN__<<"to"<<__DBL_MAX__<<endl;
+return 0;
+}
