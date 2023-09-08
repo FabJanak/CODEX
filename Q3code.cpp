c034c5f982f0c8e3c3657e5c3c2a9c9ca7ef4182
Janak Fabyani <janakfabyani30036@gmail.com>
2023-09-08 19:02:17 +0530

    Add files via upload

---
+++ Q3code.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int n1, n2, n3;
+
+    cout << "Enter the first number: ";
+    cin >> n1;
+
+    cout << "Enter the second number: ";
+    cin >> n2;
+
+    cout << "Enter the third number: ";
+    cin >> n3;
+
+    
+    if (n1 > n2 && n1 > n3) {
+        cout << "The greatest number is: " << n1 <<endl;
+    } else if (n2 > n1 && n2 > n3) {
+        cout << "The greatest number is: " << n2 << endl;
+    } else {
+        cout << "The greatest number is: " << n3 << endl;
+    }
+
+    return 0;
+}
